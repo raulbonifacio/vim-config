956cc6b078252d965bf008ff6941e6fc8afeacd2
Raul Ribeiro Bonifácio <raul_bonifacio@outlook.com>
2020-12-08 14:16:35 -0300

    - Configuration filed added.

--- config.sh
+++ config.sh
@@ -1,6 +1,17 @@
+
+! command -v git > /dev/null && echo 'Git is not installed. (Required by vim-plug)' ||  \
+! command -v node > /dev/null &&  echo 'Node JS is not installed. (Required by vim-coc)' || \
+! command -v rg > /dev/null &&  echo 'ripgrep is not installed. (Required by CocSearch)' && \
+echo 'Please install these dependencies to proceed.' && \
+exit;
+
 #Install vim-plug
 curl -fLo ./autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+#Run and configurer vim
+vim --cmd 'source ./plug.vim | PlugClean | PlugInstall | qa!';
+
 #Create the undodir
-mkdir undodir
+! test -d undodir && mkdir undodir;
+
